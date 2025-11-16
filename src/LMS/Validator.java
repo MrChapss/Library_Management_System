ca4231582d18124457c23f924fef4a621ea913a2
Chapss <palmakarsten@gmail.com>
2025-11-16 23:49:49 +0800

    Added maxium retries

--- src/LMS/Validator.java
+++ src/LMS/Validator.java
@@ -1,13 +1,27 @@
 package LMS;
 
 public class Validator {
+	private int limit = 1;
+	private final int max = 3;
+	
     public boolean Account(String name, int id){
+    	
         if (!name.equals("Mark") || id != 123){
-            System.out.println("Please input the correct info!");
+        	
+            System.out.printf("Your ID or Name is incorrect!%nPlease try again.%n%n");
+            
+            if (limit == max) {
+            	System.out.println("You have reached the maxium retries!");
+            	return false;
+            }
+            limit++;
             return true;
         } else {
         	System.out.println("Login success!");
-            return false;
-        }
+        	return false;
+        } 
+        
+        
+        
     }
 }
