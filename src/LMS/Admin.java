708c31e2b5819c9d749de91189c2cc1084e7dae7
Chapss <palmakarsten@gmail.com>
2025-11-20 22:44:38 +0800

    Good progress

--- src/LMS/Admin.java
+++ src/LMS/Admin.java
@@ -1,17 +1,32 @@
 package LMS;
 
 public class Admin extends User {
-    public void setAdminName(String name){
-        this.name = name;
-    }
 
-    public void setAdminId(int Id){
-        this.Id = Id;
-    }
-
-    public String getAdminName(){return name;}
-    public int getAdminId(){return Id;}
+    public String getName(){return name;}
+    public int getId(){return Id;}
     
+    @Override
+    public boolean checkAcc(String name, int id){
+    	
+        if (!name.equals("Admin") || id != 123){
+        	
+            System.out.printf(">>Your ID or Name is incorrect!%n");
+            
+            if (max > min) {
+            System.out.printf(">>You only have %d chance/s left%n",max);
+            }
+            
+            if (max == min) {
+            	System.out.printf("%n>>You have reached the maxium retries! Try again later.");
+            	return false;
+            }
+            max--;
+            return true;
+        } else {
+        	System.out.println("Login success!");
+        	return false;
+        }
+    }
     
 }
 
--- src/LMS/Student.java
+++ src/LMS/Student.java
@@ -1,16 +1,31 @@
 package LMS;
 
 public class Student extends User {
-    public void setStudentName(String name){
-        this.name = name;
-    }
 
-    public void setStudentId(int id){
-        this.Id = id;
-    }
-
-    public String getStudentName(){return name;}
-    public int getStudentId(){return Id;}
+    public String getName(){return name;}
+    public int getId(){return Id;}
     
-    //public String
+    @Override
+    public boolean checkAcc(String name, int id){
+    	
+        if (!name.equals("Mark Lamadora") || id != 123){
+        	
+            System.out.printf(">>Your ID or Name is incorrect!%n");
+            
+            if (max > min) {
+            System.out.printf(">>You only have %d chance/s left%n",max);
+            }
+            
+            if (max == min) {
+            	System.out.printf("%n>>You have reached the maxium retries! Try again later.");
+            	return false;
+            }
+            max--;
+            return true;
+        } else {
+        	System.out.println("Login success!");
+        	return false;
+        }
+    }
+    
 }
