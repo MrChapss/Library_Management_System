7fe0be95c3f75984280cebe61c6c80863fbe882e
Chapss <palmakarsten@gmail.com>
2026-03-04 04:01:11 +0800

    Applied the value annotation in the class

--- src/main/java/lms/security/JwtService.java
+++ src/main/java/lms/security/JwtService.java
@@ -4,9 +4,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
 @Service
 public class JwtService {
-    @Value(
+    @Value("${jwt.secretKey}")
     private String secretKey;
+    @Value("${jwt.expirationToken}")
     private long expirationToken;
-    );
-// test
 }
