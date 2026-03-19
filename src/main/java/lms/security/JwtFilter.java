8d5e5732fbb45d9db98e7dc0f45c4bffeb9c2c59
Chapss <palmakarsten@gmail.com>
2026-03-19 23:32:50 +0800

    UPDATE: Added double checker of authentication 1. The purpose is to prevent a pass through of unauthenticated user in the system 2. Every token will be authenticated unless breakthrough.

--- src/main/java/lms/security/JwtFilter.java
+++ src/main/java/lms/security/JwtFilter.java
@@ -1,8 +1,6 @@
 package lms.security;
 
 // Used to filter once per request only (to avoid multiple or flood request?)
-import lombok.Value;
-import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.web.filter.OncePerRequestFilter;
 // HttpServlet is the way of java handling incoming HTTP requests
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,7 +16,8 @@ import java.io.IOException;
 import org.springframework.stereotype.Component;
 // To get user info in database by using username
 import lms.service.AccountService;
-//
+// To secured information of the current request (prevents overwriting the filtered authentication)
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter{
@@ -31,7 +30,7 @@ public class JwtFilter extends OncePerRequestFilter{
         this.jwtService=jwtService;
         this.accountService=accountService;
     }
-    // I don't know what is this and what for
+    // Polymorphism the method of OncePerRequestFilter annotation
     @Override
     // doFilterInternal(...) where filter logic resides (similar to main method)
     protected void doFilterInternal(HttpServletRequest request,
@@ -39,20 +38,32 @@ public class JwtFilter extends OncePerRequestFilter{
                                     FilterChain filterChain
                                     ) throws ServletException, IOException{
         // Using if conditional to handle null header and not starting in bearer
+        // header is a part of a token information
         String authorizationHeader = request.getHeader("Authorization");
-            if (authorizationHeader == null ||
-                    // filtering it and pass then stop using the return.
-                    !authorizationHeader.startsWith("Bearer ")){
-                filterChain.doFilter(request, response);
-                return;
-            }
-            // prefix variable + length() = amount of index of word in a variable
-            // (in short, no need to manually type the index to remove the "Bearer " in the token)
-            String prefix = "Bearer ";
-            // substring job is to extract token using the index starting from "Bearer "
-            String token = authorizationHeader.substring(prefix.length()).trim();
-            String username = jwtService.extractUsername(token);
+        if (authorizationHeader == null ||
+                // filtering it and pass then stop using the return.
+                !authorizationHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
         }
+        // prefix variable + length() = amount of index of word in a variable
+        // (in short, no need to manually type the index to remove the "Bearer " in the token)
+        String prefix = "Bearer ";
+        // substring job is to extract token using the index starting from "Bearer "
+        String token = authorizationHeader.substring(prefix.length()).trim();
+        // extracting username to prevent a user with null username
+        String username = jwtService.extractUsername(token);
+        // SecurityContextHolder = holds the security info
+        // getContext() = container of info
+        // getAuthentication() = the object or the user
+        boolean isAuthenticatedUser = SecurityContextHolder.getContext().getAuthentication() == null;
+        if (username == null || !isAuthenticatedUser){
+            filterChain.doFilter(request, response);
+            return;
+        }
+    }
 }
 
 // need natin ma-understand this all annotation kahit comment lang natin
+// delete and repeat
+// straight forward kahit clueless, we will win
